45ad395b07739faba40c5a5ff23ad5a4d4092b5b
bportela <bportela.dev@gmail.com>
2022-04-04 20:49:37 +0200

    106. Servicio para buscar países. Devuelve un Observable<any>

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  termino: string = 'Hola mundo'
+  constructor(private paisService: PaisService) { }
 
-  ngOnInit(): void {
+  buscar(){
+    
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe(resp => console.log(resp));
+    
   }
 
 }
