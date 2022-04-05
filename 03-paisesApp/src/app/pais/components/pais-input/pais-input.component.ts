1ead2afe53f2ed8c313d5f90c3c046f8586e70b2
bportela <bportela.dev@gmail.com>
2022-04-05 10:50:30 +0200

    112. Uso de Debounce con Observable de RxJs

--- 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
@@ -6,13 +7,32 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styles: [
   ]
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit{
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  //Es un observable
+  debouncer: Subject<string> = new Subject();
+
   termino: string = '';
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(300)
+      )
+      .subscribe(valor => {
+         console.log('debouncer: ', valor);
+      })
+  }
+
+
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
+  }
   buscar(){
     this.onEnter.emit(this.termino);
   }
