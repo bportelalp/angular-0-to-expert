2a557d18f60f4ec3868bba5fba562ee1bc6881d6
bportela <bportela.dev@gmail.com>
2022-04-05 15:45:45 +0200

    inicio Seccion 9. 122 y 123 Condiciones de CSS. Varias formas de hacerlo

--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -8,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe','oceania']
+  regionActiva: string = '';
+
+  getBotonCSS(region: string){
+    return region==this.regionActiva? 'btn btn-primary m-1': 'btn btn-outline-primary m-1';
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  activarRegion(region:string){
+    this.regionActiva = region;
+
+    //TODO- LLamar al servicio
+  }
 }
