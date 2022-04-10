3604fc3fdeb196e52a4cbcaa47d27ddc065f7eb1
bportela <bportela.dev@gmail.com>
2022-04-10 19:40:01 +0200

    145. Cambiar idioma por defecto, por ejemplo, para pipes de Date

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,7 +6,10 @@ import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
-
+// Cambiar localizacionde la app
+import localES from "@angular/common/locales/es";
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localES);
 
 @NgModule({
   declarations: [
@@ -18,7 +21,12 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
