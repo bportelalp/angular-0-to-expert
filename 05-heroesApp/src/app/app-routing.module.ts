91895e8310f9cdafb677a57ccc349e248e147dd1
bportela <bportela.dev@gmail.com>
2022-04-24 16:13:44 +0200

    175. Rutas principales

---
+++ 05-heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path:'notfound',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound'
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
