d7cbc5b5596590c81e6d57f047ceafd82098a041
bportela <bportela.dev@gmail.com>
2022-06-20 22:34:51 +0200

    189. Interfaz heroe y visibilización con ngFor

--- 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
@@ -9,11 +10,15 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class ListadoComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor(private heroesService: HeroesService) {  }
 
   ngOnInit(): void {
     this.heroesService.getHeroes()
-      .subscribe( resp => console.log(resp));
+      .subscribe( heroes => {
+        this.heroes = heroes;
+      });
   }
 
 }
